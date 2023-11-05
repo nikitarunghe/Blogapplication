012091ad15859e543ba229b35f4ba2d6e0a1b648
Nikita Runghe <nikitarunghe94@gmail.com>
2023-11-05 14:45:13 +0530

    initial commit

---
+++ controller/image-controller.js
@@ -0,0 +1,61 @@
+import grid from 'gridfs-stream';
+import mongoose from 'mongoose';
+ import { GridFSBucket } from 'mongodb';
+
+ const url = '';
+
+
+ let gfs, gridfsBucket;
+const conn = mongoose.connection;
+conn.once('open', () => {
+    gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+        bucketName: 'fs'
+    });
+     gfs = grid(conn.db, mongoose.mongo);
+    gfs.collection('fs');  
+   
+});
+
+
+
+export const uploadImage = (request, response) => {
+    if(!request.file) {
+        return response.status(404).json("File not found");
+    }
+    const imageUrl = `${url}/file/${request.file.filename}`;
+
+    return response.status(200).json(imageUrl);    
+}
+
+
+
+
+export const getImage = async (request, response) => {
+  const filename = request.params.filename;
+
+  if (!filename) {
+    return response.status(400).json({ msg: "Missing filename parameter" });
+  }
+
+  try {
+    const file = await gfs.files.findOne({ filename });
+
+    if (!file) {
+      return response.status(404).json({ msg: "File not found" });
+    }
+
+    const fileId = file._id;
+    const readStream = gridfsBucket.openDownloadStream(fileId);
+
+    readStream.pipe(response);
+  } catch (error) {
+    response.status(500).json({ msg: error.message });
+  }
+};
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import  Connection from './database/db.js';
+import Router from './routes/route.js' ;
+import cors from 'cors';
+import bodyParser  from 'body-parser';
+import path from 'path';
+
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/',Router);
+
+
+
+app.use(express.static(path.join(__dirname,"./client/build")));
+app.get('*',function(_,res){
+           res.sendFile(path.join(__dirname,"./client/build/index.html"),function(err){
+              res.status(500).send(err);
+           })
+})
+dotenv.config();
+const PORT =  process.env.PORT||8000;
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+
+
+Connection(USERNAME,PASSWORD);
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
