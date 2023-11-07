3f03f5596b9bd36530594374990988930573fc8d
Nikita Runghe <nikitarunghe94@gmail.com>
2023-11-07 11:17:55 +0530

    modify

--- server.js
+++ server.js
@@ -38,6 +38,8 @@
 // });
 
 // app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+import express from 'express';
+import dotenv from 'dotenv';
 import Connection from
  
 './database/db.js';
