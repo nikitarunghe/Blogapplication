9d9c99e2d995b4939ac6ae06214f1977faa9aac0
Nikita Runghe <nikitarunghe94@gmail.com>
2023-11-08 15:29:03 +0530

    db.js

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
- 
+ dotenv.config();
    const Connection = async () => {
         const URL =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pn9ukdy.mongodb.net/?retryWrites=true&w=majority`;
   
